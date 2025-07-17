9ec1c56d4ef67d442be3c350e2a407702b5cc55f
duzqiu <duzqiu@outlook.com>
2025-07-17 11:40:02 +0800

    555

--- main.py
+++ main.py
@@ -5,11 +5,34 @@ from kivymd.uix.swiper import MDSwiper, MDSwiperItem
 from kivy.core.window import Window
 from kivy.clock import Clock
 
+from kivymd.uix.boxlayout import MDBoxLayout
+from kivy.properties import StringProperty
+
 Window.size = (375, 667)
 
+class MenuItem(MDBoxLayout):
+    icon = StringProperty("")
+    text = StringProperty("")
+
+    def on_touch_down(self, touch):
+        if self.collide_point(*touch.pos):
+            print(f"点击了：{self.text}")
+            return True
+        return super().on_touch_down(touch)
+
+class MyGridItem(MDBoxLayout):
+    icon = StringProperty("")
+    text = StringProperty("")
+
+    def on_touch_down(self, touch):
+        if self.collide_point(*touch.pos):
+            print(f"点击了：{self.text}")
+            return True
+        return super().on_touch_down(touch)
+    
 class MainApp(MDApp):
     def build(self):
-        self.root = Builder.load_file("main.kv")
+        self.root = Builder.load_file("kv/main.kv")
         self.add_swiper_items()
         return self.root
     
@@ -20,7 +43,7 @@ class MainApp(MDApp):
                         allow_stretch=True,
                         fit_mode="fill")  # 中心定位)
             swiper_item.add_widget(img)
-            self.root.ids.swiper.add_widget(swiper_item)
+            self.root.ids.home.ids.swiper.add_widget(swiper_item)
     def on_scan(self):
         print("点击了：扫一扫")
 
@@ -29,6 +52,15 @@ class MainApp(MDApp):
 
     def on_settings(self):
         print("点击了：设置")
-    
+
+    def on_search(self):
+        # 获取输入内容
+        input_text = self.root.ids.search_input.text.strip()
+        print("用户输入了：", input_text)
+        # 可以在这里触发搜索逻辑
+    def on_login_label_click(self):
+        print("点击了登录/注册")
+        # 打开登录页面或弹出对话框
+
 if __name__ == '__main__':
     MainApp().run()
